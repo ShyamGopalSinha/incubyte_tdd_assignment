543f7fccc0a739c7d02f493ee867436e70dc7fd7
ShyamGopalSinha <shyam.gopal8445220743@gmail.com>
2025-10-29 12:29:14 +0530

    my third passed test case

--- spec/my_file_spec.rb
+++ spec/my_file_spec.rb
@@ -7,5 +7,8 @@ RSpec.describe MyFile do
     it "returns number for single input" do
       expect(MyFile.add("1")).to eq(1)
     end
+    it "returns the sum of two numbers separated by a comma" do
+      expect(MyFile.add("1,2")).to eq(3)
+    end
   end
 end
