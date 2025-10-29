dc599b2f14772ec57721acaf6b08a3945e24c7df
ShyamGopalSinha <shyam.gopal8445220743@gmail.com>
2025-10-29 13:03:41 +0530

    my sixth passed test case

--- lib/my_file.rb
+++ lib/my_file.rb
@@ -1,6 +1,12 @@
 class MyFile
     def self.add(numbers)
         return 0 if numbers.empty?
-        numbers.split(/[\s,]+/).map(&:to_i).sum
+        delimiters = [",", "\n"]
+        if numbers.start_with?("//")
+        delimiter_section, numbers = numbers.split("\n", 2)
+        custom_delimiter = delimiter_section[2] 
+        delimiters = [custom_delimiter]
+        end
+        parts = numbers.split(Regexp.union(delimiters)).map(&:to_i).sum
     end
 end
