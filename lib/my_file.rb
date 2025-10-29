8c9537f1c9267c4d6f8b919cc7d15e688e726965
ShyamGopalSinha <shyam.gopal8445220743@gmail.com>
2025-10-29 12:44:44 +0530

    my fifth passed test case

--- lib/my_file.rb
+++ lib/my_file.rb
@@ -1,6 +1,6 @@
 class MyFile
     def self.add(numbers)
         return 0 if numbers.empty?
-        numbers.split(',').map(&:to_i).sum
+        numbers.split(/[\s,]+/).map(&:to_i).sum
     end
 end
