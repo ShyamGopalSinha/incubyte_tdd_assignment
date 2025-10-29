3d79e9083b67a670f385406b704e30af096bed9e
ShyamGopalSinha <shyam.gopal8445220743@gmail.com>
2025-10-29 13:09:33 +0530

    my seventh failed test case

--- spec/my_file_spec.rb
+++ spec/my_file_spec.rb
@@ -19,5 +19,10 @@ RSpec.describe MyFile do
     it "supports custom single-character delimiter" do
       expect(MyFile.add("//;\n1;2")).to eq(3)
     end
+    it "raises an exception when negative numbers are present" do
+     expect {
+       MyFile.add("1,-2,3,-5")
+     }.to raise_error("negatives not allowed: -2, -5")
+   end
   end
 end
